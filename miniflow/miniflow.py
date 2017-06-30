0a62e334486e3d65fcf37626f03b91c045b78c46
tobe <tobeg3oogle@gmail.com>
2017-06-30 17:00:27 +0800

    Remove CoefficientOp and refactor VariableOp

--- miniflow/miniflow.py
+++ miniflow/miniflow.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python
+# Copyright 2017 The Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import client
 import ops
@@ -19,8 +31,8 @@ def main():
   print("Formula gradient: {}".format("y' = 9 * x**2 + 4 * x + 1"))
 
   x = 1
-  first_item = ops.MultipleOp(ops.CoefficientOp(3), ops.CubicOp(x))
-  second_item = ops.MultipleOp(ops.CoefficientOp(2), ops.SquareOp(x))
+  first_item = ops.MultipleOp(3, ops.CubicOp(x))
+  second_item = ops.MultipleOp(2, ops.SquareOp(x))
   third_item = ops.VariableOp(x)
   forth_item = ops.ConstantOp(10)
   y = ops.AddNOp(ops.AddOp(first_item, second_item), third_item, forth_item)
@@ -30,9 +42,9 @@ def main():
 
   # Build the graph
   name_op_map = {}
-  coefficient1 = ops.CoefficientOp(3)
+  coefficient1 = ops.ConstantOp(3)
   cubic1 = ops.CubicOp(x)
-  coefficient2 = ops.CoefficientOp(2)
+  coefficient2 = ops.ConstantOp(2)
   square1 = ops.SquareOp(x)
   multiple1 = ops.MultipleOp(coefficient1, cubic1)
   multiple2 = ops.MultipleOp(coefficient2, square1)
@@ -57,43 +69,6 @@ def main():
   print("add2 forward: {}, backward: {}".format(name_op_map["add2"].forward(),
                                                 name_op_map["add2"].grad()))
 
-  # Build the grapah with placeholer
-  name_op_map = {}
-  x = ops.PlaceholderOp()
-  coefficient1 = ops.CoefficientOp(3)
-  cubic1 = ops.CubicOp(x)
-  coefficient2 = ops.CoefficientOp(2)
-  square1 = ops.SquareOp(x)
-  multiple1 = ops.MultipleOp(coefficient1, cubic1)
-  multiple2 = ops.MultipleOp(coefficient2, square1)
-  variable1 = ops.VariableOp(x)
-  constant1 = ops.ConstantOp(10)
-  add1 = ops.AddNOp(multiple1, multiple2, variable1, constant1)
-  add2 = ops.AddOp(multiple1, multiple2)
-
-  name_op_map["coefficient1"] = coefficient1
-  name_op_map["cubic1"] = cubic1
-  name_op_map["coefficient2"] = coefficient2
-  name_op_map["square1"] = square1
-  name_op_map["multiple1"] = multiple1
-  name_op_map["multiple2"] = multiple2
-  name_op_map["variable1"] = variable1
-  name_op_map["constant1"] = constant1
-  name_op_map["add1"] = add1
-  name_op_map["add2"] = add2
-
-  x.set_value(1)
-  print("Placeholder: {}, add1 forward: {}, backward: {}".format(
-      1, name_op_map["add1"].forward(), name_op_map["add1"].grad()))
-  print("Placeholder: {}, add2 forward: {}, backward: {}".format(
-      1, name_op_map["add2"].forward(), name_op_map["add2"].grad()))
-
-  x.set_value(2)
-  print("Placeholder: {}, add1 forward: {}, backward: {}".format(
-      2, name_op_map["add1"].forward(), name_op_map["add1"].grad()))
-  print("Placeholder: {}, add2 forward: {}, backward: {}".format(
-      2, name_op_map["add2"].forward(), name_op_map["add2"].grad()))
-
   # Automatically update weights with gradient
   learning_rate = 0.01
   epoch_number = 10
@@ -102,46 +77,28 @@ def main():
 
   # y = weights * x**3 + 2 * x**2 + x + 10
   x = ops.PlaceholderOp()
-  weights = ops.CoefficientOp(weights_value)
+  weights = ops.VariableOp(weights_value)
   cubic1 = ops.CubicOp(x)
-  coefficient2 = ops.CoefficientOp(2)
+  coefficient2 = ops.ConstantOp(2)
   square1 = ops.SquareOp(x)
   multiple1 = ops.MultipleOp(weights, cubic1)
   multiple2 = ops.MultipleOp(coefficient2, square1)
-  variable1 = ops.VariableOp(x)
   constant1 = ops.ConstantOp(10)
-  add1 = ops.AddNOp(multiple1, multiple2, variable1, constant1)
+  add1 = ops.AddNOp(multiple1, multiple2, x, constant1)
 
   for epoch_index in range(epoch_number):
 
     x.set_value(1)
+    #import ipdb;ipdb.set_trace()
 
     grad = add1.grad()
     weights_value -= learning_rate * grad
-    weights.set_x(weights_value)
+    weights.set_value(weights_value)
 
     predict = add1.forward()
     loss = predict - label
     print("Epoch: {}, loss: {}, grad: {}, weights: {}, predict: {}".format(
         epoch_index, loss, grad, weights_value, predict))
-
-  # Run with default graph
-  def myfunction_python(x):
-    return x**2 + x
-
-  def myfunction(x):
-    placeholer1 = ops.PlaceholderOp()
-    # TODO: Set value after constructing the graph
-    placeholer1.set_value(x)
-    square1 = ops.SquareOp(placeholer1)
-    variable1 = ops.VariableOp(placeholer1)
-    add1 = ops.AddOp(square1, variable1)
-    return add1.name
-
-  myfunction_op_name = myfunction(10)
-  myfunction_op = client.get_default_graph().name_op_map[myfunction_op_name]
-  print("Run myfunction: {}, auto grad of myfucntion: {}".format(
-      myfunction_op.forward(), myfunction_op.grad()))
 
   # Run with session
   ''' TensorFlow example
--- miniflow/ops.py
+++ miniflow/ops.py
@@ -1,16 +1,34 @@
+# Copyright 2017 The Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This module contains all the basic operations."""
+
 import math
 import numpy as np
 import os
 import sys
 
-import client
+import graph
 
+# Enable swig by environment variable
 if os.environ.has_key("ENABLE_SWIG_OP"):
   sys.path.append("../")
   import swig.op
 
 
 class Op(object):
+  """The basic class for all operation."""
+
   def __init__(self):
     pass
 
@@ -22,16 +40,19 @@ class Op(object):
 
 
 class PlaceholderOp(Op):
-  def __init__(self, dtype=None, name=None):
-    if name == None:
-      self.name = "Placeholder"
-    else:
-      self.name = name
+  """The placeholer operation which value is set when Session.run()"""
 
+  def __init__(self, dtype=None, shape=None, name="Placeholder"):
+    # TODO: Use dtype and shape
     self.dtype = dtype
+    self.shape = shape
+    self.name = name
+
+    # The value is None util Session.run() with feed_dict parameter
     self.value = None
 
-    self.graph = client.get_default_graph()
+    # TODO: Support other graph instance
+    self.graph = graph.get_default_graph()
     self.graph.add_to_graph(self)
 
   def set_value(self, value):
@@ -47,82 +68,54 @@ class PlaceholderOp(Op):
     return 0
 
 
-# Notice: ConstantOp is different from the one of TensorFlow
 class ConstantOp(Op):
-  def __init__(self, x, name=None):
-    if name == None:
-      self.name = "Constant"
-    else:
-      self.name = name
+  """The constant operation which contains one initialized value."""
 
-    # Notice, self.x should be Number or PlaceholerOp
-    self.x = x
+  def __init__(self, value, name="Constant"):
+    self.value = value
+    self.name = name
 
-    self.graph = client.get_default_graph()
+    self.graph = graph.get_default_graph()
     self.graph.add_to_graph(self)
 
+  # TODO: Not allow to set the value
+
+  def get_value(self):
+    return self.value
+
   def forward(self):
-    if isinstance(self.x, PlaceholderOp):
-      x = self.x.get_value()
-    else:
-      x = self.x
-    return x
+    return self.value
 
   def grad(self):
     return 0
 
 
-class CoefficientOp(Op):
-  def __init__(self, x, name=None):
-    if name == None:
-      self.name = "Coefficient"
-    else:
-      self.name = name
-
-    self.x = x
-
-    self.graph = client.get_default_graph()
-    self.graph.add_to_graph(self)
-
-  def set_x(self, x):
-    self.x = x
-
-  def get_x(self):
-    return self.x
-
-  def forward(self):
-    if isinstance(self.x, PlaceholderOp):
-      x = self.x.get_value()
-    else:
-      x = self.x
-    return x
-
-  def grad(self):
-    if isinstance(self.x, PlaceholderOp):
-      x = self.x.get_value()
-    else:
-      x = self.x
-    return x
-
-
 class VariableOp(Op):
-  def __init__(self, x, name=None):
-    if name == None:
-      self.name = "Variable"
-    else:
-      self.name = name
+  """
+  The variable operation which contains one variable. The variable may be
+  trainable or not-trainable. This is used to define the machine learning
+  models.
+  """
 
-    self.x = x
+  def __init__(self, value, is_trainable=True, name="Variable"):
+    self.value = value
+    self.is_trainable = is_trainable
+    self.name = name
 
-    self.graph = client.get_default_graph()
+    self.graph = graph.get_default_graph()
     self.graph.add_to_graph(self)
 
+    if self.is_trainable:
+      self.graph.add_to_trainable_variables_collection(self.name, self)
+
+  def get_value(self):
+    return self.value
+
+  def set_value(self, value):
+    self.value = value
+
   def forward(self):
-    if isinstance(self.x, PlaceholderOp):
-      x = self.x.get_value()
-    else:
-      x = self.x
-    return x
+    return self.value
 
   def grad(self):
     return 1
@@ -136,35 +129,26 @@ def test_VariableOp():
 
 
 class SquareOp(Op):
-  def __init__(self, x, name=None):
-    if name == None:
-      self.name = "Square"
+  def __init__(self, input, name="Square"):
+    if not isinstance(input, Op):
+      self.op = ConstantOp(input)
     else:
-      self.name = name
+      self.op = input
 
-    self.x = x
+    self.name = name
 
-    self.graph = client.get_default_graph()
+    self.graph = graph.get_default_graph()
     self.graph.add_to_graph(self)
 
   def forward(self):
-    if isinstance(self.x, PlaceholderOp):
-      x = self.x.get_value()
-    else:
-      x = self.x
-
     if os.environ.has_key("ENABLE_SWIG_OP"):
-      result = swig.op.square(x)
+      result = swig.op.square(self.op.forward())
     else:
-      result = pow(x, 2)
+      result = pow(self.op.forward(), 2)
     return result
 
   def grad(self):
-    if isinstance(self.x, PlaceholderOp):
-      x = self.x.get_value()
-    else:
-      x = self.x
-    return 2 * x
+    return 2 * self.op.forward()
 
 
 def test_SquareOp():
@@ -175,39 +159,29 @@ def test_SquareOp():
 
 
 class CubicOp(Op):
-  def __init__(self, x, name=None):
-    if name == None:
-      self.name = "Cubic"
+  def __init__(self, input, name="Cubic"):
+    if not isinstance(input, Op):
+      self.op = ConstantOp(input)
     else:
-      self.name = name
+      self.op = input
 
-    self.x = x
+    self.name = name
 
-    self.graph = client.get_default_graph()
+    self.graph = graph.get_default_graph()
     self.graph.add_to_graph(self)
 
   def forward(self):
-    if isinstance(self.x, PlaceholderOp):
-      x = self.x.get_value()
-    else:
-      x = self.x
-
     if os.environ.has_key("ENABLE_SWIG_OP"):
-      result = swig.op.cubic(x)
+      result = swig.op.cubic(self.op.forward())
     else:
-      result = math.pow(x, 3)
+      result = math.pow(self.op.forward(), 3)
     return result
 
   def grad(self):
-    if isinstance(self.x, PlaceholderOp):
-      x = self.x.get_value()
-    else:
-      x = self.x
-
     if os.environ.has_key("ENABLE_SWIG_OP"):
-      result = swig.op.multiple(3, swig.op.square(x))
+      result = swig.op.multiple(3, swig.op.square(self.op.forward()))
     else:
-      result = 3 * math.pow(x, 2)
+      result = 3 * math.pow(self.op.forward(), 2)
     return result
 
 
@@ -219,15 +193,11 @@ def test_CubicOp():
 
 
 def SigmoidOp(x):
-  def __init__(self, x, name=None):
-    if name == None:
-      self.name = "Sigmoid"
-    else:
-      self.name = name
-
+  def __init__(self, x, name="Sigmoid"):
+    self.name = name
     self.x = x
 
-    self.graph = client.get_default_graph()
+    self.graph = graph.get_default_graph()
     self.graph.add_to_graph(self)
 
   def forward(self):
@@ -245,15 +215,47 @@ def SigmoidOp(x):
     return forward(x) / (1 - forward(x))
 
 
-# TODO: Composite op only supports linear combination
-class AddOp(Op):
-  def __init__(self, op1, op2):
-    self.name = "Add"
-    self.op1 = op1
-    self.op2 = op2
-    self.name = None
+'''
+def get_real_value(input):
+  """Get the real value of the operation.
 
-    self.graph = client.get_default_graph()
+  Args:
+    input: May be primitive, ConstantOp, PlaceholerOp or VariableOp.
+    
+  Returns:
+    The primitive value from the input operation.
+  """
+  if isinstance(input, ConstantOp) or isinstance(input, PlaceholderOp) or isinstance(input, VariableOp):
+    value = input.get_value()
+  else:
+    value = input
+  return value
+
+def test_get_real_value():
+  print(get_real_value(10))
+'''
+
+
+class AddOp(Op):
+  """
+  The addition operation which has only two inputs. The input can be
+  primitive, ConstantOp, PlaceholerOp, VariableOp or other ops.
+  """
+
+  def __init__(self, input1, input2, name="Add"):
+    if not isinstance(input1, Op):
+      self.op1 = ConstantOp(input1)
+    else:
+      self.op1 = input1
+
+    if not isinstance(input2, Op):
+      self.op2 = ConstantOp(input2)
+    else:
+      self.op2 = input2
+
+    self.name = name
+
+    self.graph = graph.get_default_graph()
     self.graph.add_to_graph(self)
 
   def forward(self):
@@ -266,13 +268,17 @@ class AddOp(Op):
 
 
 class AddNOp(Op):
-  def __init__(self, *ops):
+  def __init__(self, *inputs):
     # TODO: Support user defined name in the parameter
     self.name = "AddN"
-    self.ops = ops
-    self.name = None
 
-    self.graph = client.get_default_graph()
+    self.ops = []
+    for input in inputs:
+      if not isinstance(input, Op):
+        input = ConstantOp(input)
+      self.ops.append(input)
+
+    self.graph = graph.get_default_graph()
     self.graph.add_to_graph(self)
 
   def forward(self):
@@ -290,13 +296,20 @@ class AddNOp(Op):
 
 # TODO: Can not support operations like "x * x", only "x * 3"
 class MultipleOp(Op):
-  def __init__(self, op1, op2):
-    self.name = "Multiple"
-    self.op1 = op1
-    self.op2 = op2
-    self.name = None
+  def __init__(self, input1, input2, name="Multiple"):
+    if not isinstance(input1, Op):
+      self.op1 = ConstantOp(input1)
+    else:
+      self.op1 = input1
 
-    self.graph = client.get_default_graph()
+    if not isinstance(input2, Op):
+      self.op2 = ConstantOp(input2)
+    else:
+      self.op2 = input2
+
+    self.name = name
+
+    self.graph = graph.get_default_graph()
     self.graph.add_to_graph(self)
 
   def forward(self):
@@ -309,12 +322,16 @@ class MultipleOp(Op):
 
 
 class MultipleNOp(Op):
-  def __init__(self, *ops):
+  def __init__(self, *inputs):
     self.name = "MultipleN"
-    self.ops = ops
-    self.name = None
 
-    self.graph = client.get_default_graph()
+    self.ops = []
+    for input in inputs:
+      if not isinstance(input, Op):
+        input = ConstantOp(input)
+      self.ops.append(input)
+
+    self.graph = graph.get_default_graph()
     self.graph.add_to_graph(self)
 
   def forward(self):
@@ -328,3 +345,34 @@ class MultipleNOp(Op):
     for op in self.ops:
       result *= op.grad()
     return result
+
+
+class UpdateVariableOp(Op):
+  def __init__(self, variableOp, value, name="UpdateVariableOp"):
+    self.variableOp = variableOp
+    self.value = value
+    self.name = name
+
+    self.graph = graph.get_default_graph()
+    self.graph.add_to_graph(self)
+
+  def forward(self):
+    self.variableOp.set_value(self.value)
+    return self.value
+
+  # TODO: Add grad() if needed
+
+
+class UpdateVariableNOp(Op):
+  def __init__(self, variableop_value_map, name="UpdateVariableNOp"):
+    self.variableop_value_map = variableop_value_map
+    self.name = name
+
+    self.graph = graph.get_default_graph()
+    self.graph.add_to_graph(self)
+
+  def forward(self):
+    # TODO: Need to test the loop
+    for variableOp, value in enumerate(self.variableop_value_map):
+      variableOp.set_value(value)
+    return self.variableop_value_map
