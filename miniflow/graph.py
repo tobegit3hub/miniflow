66a7a3e6d3b4abe91319682813e5cc35ac9349bf
tobe <tobeg3oogle@gmail.com>
2017-06-30 17:16:59 +0800

    Refactor to seperate graph and session in client.py

---
+++ miniflow/graph.py
@@ -0,0 +1,56 @@
+# Copyright 2017 The Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class Graph(object):
+  def __init__(self):
+    self.name_op_map = {}
+
+    self.trainable_variables_collection = {}
+
+  def get_unique_name(self, original_name):
+    index = 0
+    unique_name = "{}_{}".format(original_name, index)
+
+    while unique_name in self.name_op_map.keys():
+      index += 1
+      unique_name = "{}_{}".format(original_name, index)
+
+    return unique_name
+
+  def add_to_graph(self, op):
+    op.name = self.get_unique_name(op.name)
+    self.name_op_map[op.name] = op
+
+  def get_trainable_variables_collection(self):
+    return self.trainable_variables_collection
+
+  def add_to_trainable_variables_collection(self, key, value):
+    if self.trainable_variables_collection.has_key(key):
+      # TODO: Using logging
+      print("Warning, the key exists")
+    else:
+      self.trainable_variables_collection[key] = value
+
+
+# TODO: Make global variable for all packages
+default_graph = Graph()
+
+
+def get_default_graph():
+  if default_graph == None:
+    global default_graph
+    default_graph = Graph()
+  else:
+    return default_graph
---
+++ miniflow/session.py
@@ -0,0 +1,38 @@
+# Copyright 2017 The Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import ops
+
+
+class Session(object):
+  def __init__(self):
+    pass
+
+  def run(self, op, feed_dict=None, options=None):
+
+    # Update the value of PlaceholerOp with feed_dict data
+    name_op_map = op.graph.name_op_map
+
+    if feed_dict != None:
+      # Example: {"Placeholer_1": 10} or {PlaceholderOp: 10}
+      for op_or_opname, value in feed_dict.items():
+        if isinstance(op_or_opname, str):
+          placeholder_op = name_op_map[op_or_opname]
+        else:
+          placeholder_op = op_or_opname
+        if isinstance(placeholder_op, ops.PlaceholderOp):
+          placeholder_op.set_value(value)
+
+    result = op.forward()
+    return result
