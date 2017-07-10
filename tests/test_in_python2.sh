ad6b109772d070808475acf6c5901cbddfd7c1aa
tobe <tobeg3oogle@gmail.com>
2017-07-10 09:50:52 +0800

    Add script to test in python2 and python3

---
+++ tests/test_in_python2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker run -it -v /:/host python:2 bash
+
+# python --version
+# pip install miniflow
---
+++ tests/test_in_python3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker run -it -v /:/host python:3 bash
+
+# python --version
+# pip install miniflow
